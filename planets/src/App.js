e7e31ca937ca2b5acd7c645e71173ccbaa6f3de2
Shyji John <shyjijohn90@gmail.com>
2024-02-07 23:05:56 +0000

    mercury, venus , earth and moon animated.

--- planets/src/App.js
+++ planets/src/App.js
@@ -3,26 +3,28 @@ import './App.css';
 function App() {
 
   return (
-    <div class="body">
+    <div >
 
-      <h1 class="heading">SOLAR SYSTEM</h1>
+      {/* <h1 class="heading">SOLAR SYSTEM</h1> */}
 
-      <div class="planets">
-      <div class="sun"></div>
+      {/* <div class="planets"> */}
+        <div class="sun"></div>
 
-      <div class="mercury">
-        <img src ="./image/saturn.jpg"></img>
-      </div>
-      <div class="venus"></div>
-      <div class="earth">
-      <div class="moon"></div>
-      </div>
-      <div class="mars"></div>
-      <div class="jupiter"></div>
-      <div class="saturn"></div>
-      <div class="uranus"></div>
-      <div class="neptune"></div>
-    </div>
+        <div class="mercury"/>
+        <div class="venus"></div>
+        <div class="earth">
+          <div class="moon"></div>
+        </div>
+        {/* <div class="venus"></div>
+        <div class="earth">
+          <div class="moon"></div>
+        </div>
+        <div class="mars"></div>
+        <div class="jupiter"></div>
+        <div class="saturn"></div>
+        <div class="uranus"></div>
+        <div class="neptune"></div> */}
+      {/* </div> */}
     </div>
   );
 }
