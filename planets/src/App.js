8c802bb72924e11dc821ced9620805fdefbeeb31
Shyji John <shyjijohn90@gmail.com>
2024-02-07 12:57:04 +0000

    solar system with unique color of planets

---
+++ planets/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div class="body">
+
+      <h1 class="heading">SOLAR SYSTEM</h1>
+
+      <div class="planets">
+      <div class="sun"></div>
+
+      <div class="mercury">
+        <img src ="./image/saturn.jpg"></img>
+      </div>
+      <div class="venus"></div>
+      <div class="earth">
+      <div class="moon"></div>
+      </div>
+      <div class="mars"></div>
+      <div class="jupiter"></div>
+      <div class="saturn"></div>
+      <div class="uranus"></div>
+      <div class="neptune"></div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
+
